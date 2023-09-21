d40ac739c96749a05595f9a9f55b89555621f8c4
unknown <damianalcalayy@gmail.com>
2023-09-21 21:38:13 +0100

    added orders and products

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { DropdownMenu, DropdownMenuContent } from "@radix-ui/react-dropdown-menu"
+import { ProductColumn } from "./columns"
+import { DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react"
+import toast from "react-hot-toast"
+import { useParams, useRouter } from "next/navigation"
+import { useState } from "react"
+import axios from "axios"
+import { AlertModal } from "@/components/modals/alert-modal"
+
+
+interface CellActionProps{
+    data: ProductColumn
+}
+
+export const CellAction: React.FC<CellActionProps> = ({
+    data
+}) => {
+    const router = useRouter()
+    const params = useParams()
+    const onCopy = (id: string) => {
+        navigator.clipboard.writeText(id);
+        toast.success("Product ID copied to the clipboard.")
+    }
+
+    const [loading, setLoading] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const onDelete = async () => {
+        try {
+            setLoading(true)
+            await axios.delete(`/api/${params.storeId}/products/${data.id}`)
+            router.refresh()
+            toast.success("Product deleted.")
+        } catch (error) {
+            toast.error("Something went wrong.")
+        } finally {
+            setLoading(false)
+            setOpen(false)
+        }
+    }
+    
+    return (
+        <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={onDelete}
+                loading={loading}
+            />
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open Menu</span>
+                        <MoreHorizontal className="w-4 h-4"/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>
+                        Actions
+                    </DropdownMenuLabel>
+                    <DropdownMenuItem onClick={() => onCopy(data.id)}>
+                        <Copy className="mr-2 h-4 w-4"/>
+                        Copy ID
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => router.push(`/${params.storeId}/products/${data.id}`)}>
+                        <Edit className="mr-2 h-4 w-4"/>
+                        Update
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setOpen(true)}>
+                        <Trash className="mr-2 h-4 w-4"/>
+                        Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -32,6 +32,21 @@ export function MainNav({
             active: pathname === `/${params.storeId}/sizes`
         },
         {
+            href: `/${params.storeId}/colors`,
+            label: 'Colors',
+            active: pathname === `/${params.storeId}/colors`
+        },
+        {
+            href: `/${params.storeId}/products`,
+            label: 'Products',
+            active: pathname === `/${params.storeId}/products`
+        },
+        {
+            href: `/${params.storeId}/orders`,
+            label: 'Orders',
+            active: pathname === `/${params.storeId}/orders`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`
